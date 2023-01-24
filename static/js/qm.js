b144d36ee9fdffcd80d947d5a85d44878b2bca99
Juan Arango <juanfer_arango@hotmail.com>
2023-01-23 19:48:46 -0500

    eventos-primer evento real

--- static/js/qm.js
+++ static/js/qm.js
@@ -1,10 +1,13 @@
 inscribirse = () => {
     var x = document.getElementById("inscribirse");
     var y = document.getElementById("btn-inscribir");
+    var z = document.getElementById("btn-cancelar");
     if (x.style.display === "none") {
         x.style.display = "flex";
+        z.style.display = "flex";
     } else {
         x.style.display = "none";
+        z.style.display = "none";
     }
     if (y.style.display === "none") {
         y.style.display = "block";
