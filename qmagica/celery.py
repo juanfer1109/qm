2373a8c166508ad28b2c480b1c7575f03a424b63
Juan Arango <juanfer_arango@hotmail.com>
2023-02-02 20:57:07 -0500

    cambiar nombre a qmagica

--- qmagica/celery.py
+++ qmagica/celery.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Quintana.settings')
-app = Celery('Quintana')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qmagica.settings')
+app = Celery('qmagica')
 app.conf.enable_utc=False
 app.conf.update(timezone='America/Bogota')
 app.config_from_object('django.conf:settings', namespace='CELERY')
