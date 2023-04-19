aae05638bc7cb5c48e211fe3af588b54ebe3127e
Juan Arango <juanfer_arango@hotmail.com>
2023-04-19 12:42:55 -0500

    Promocion de eventos

--- eventos/tasks.py
+++ eventos/tasks.py
@@ -33,3 +33,32 @@ def RecordarPago(token):
         email.attach_alternative(content, 'text/html')
         email.fail_silently = False
         email.send()
+
+@shared_task(bind=True)
+def PromocionarEventos(token):
+    talleres = Evento.objects.filter(cancelado=False, concluido=False, cerrado=False)
+    if len(talleres) > 0:
+        lista_correo = CustomUser.objects.filter(mailing_list=True, comunidad=False)
+
+        for taller in talleres:
+            subject = 'Invitación al evento ' + taller.nombre
+            inscritos = []
+            for inscrito in Inscripciones.objects.filter(evento=taller):
+                inscritos.append(inscrito.usuario)
+            for persona in lista_correo:
+                if User.objects.get(customuser=persona) not in inscritos:
+                    template = get_template('eventos/email_invitacion_evento.html')
+                    content = template.render({
+                    'usuario': persona,
+                    'evento': taller,
+                    })
+                    sendTo = User.objects.get(username=persona.user).email
+                    email = EmailMultiAlternatives(
+                    subject,
+                    '',
+                    settings.EMAIL_HOST_USER,
+                    [sendTo,],
+                    )
+                    email.attach_alternative(content, 'text/html')
+                    email.fail_silently = False
+                    email.send()
